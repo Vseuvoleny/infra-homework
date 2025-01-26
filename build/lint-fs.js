4245e9776e0b3e8d5f3b82cbf4444ed679c1dfc3
Санталов Артур <arthur@MacBook-Pro-Santalov.local>
2025-01-26 16:15:13 +0400

    task -3

--- build/lint-fs.js
+++ build/lint-fs.js
@@ -1,2 +1,32 @@
 import fs from 'node:fs/promises';
 import path from 'node:path';
+
+async function lint() {
+  const pathToFiles = path.resolve(import.meta.dirname, '../src/components');
+  const arrOfError = [];
+  const folders = await fs.readdir(path.resolve(pathToFiles));
+  for (const folder of folders) {
+    const result = await fs.readdir(path.resolve(`${pathToFiles}/${folder}`));
+    const indexFileName = 'index.js';
+    const folderName = `${folder}.js`;
+    if (!result.includes(indexFileName)) {
+      arrOfError.push(
+        `Structure of component ${folder} is broken! Missing ${indexFileName}`,
+      );
+    }
+    if (!result.includes(`${folder}.js`)) {
+      arrOfError.push(
+        `Structure of component ${folder} is broken! Missing ${folderName}`,
+      );
+    }
+  }
+  if (arrOfError.length === 0) {
+    console.log('File structure if right');
+  } else {
+    arrOfError.forEach((e) => {
+      console.log(e);
+    });
+  }
+}
+
+lint();
